1dc1927a0ba6bb001af000dcc98db80225a12f61
Amarjeet <jeetcodercpp@gmail.com>
2022-03-20 10:01:26 +0530

    Easy

---
+++ Simple_Input.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+	int n;
+	int sum=0;
+	
+	while(sum>=0){
+	
+     	cin>>n;
+		
+		sum = sum + n;
+        if(sum>=0)
+        cout<<n<<endl;
+           
+	}
+
+	return 0;
+}
