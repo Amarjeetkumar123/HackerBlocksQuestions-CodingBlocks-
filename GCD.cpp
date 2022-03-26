ec92faad07002ec9caf20e55d94ee44a66f29731
Amarjeet <jeetcodercpp@gmail.com>
2022-03-27 00:34:34 +0530

    Another methode

--- GCD.cpp
+++ GCD.cpp
@@ -21,3 +21,31 @@ int main() {
 
   return 0;
 }
+
+
+// Another Methode......
+
+#include<iostream>
+using namespace std;
+int gdc(int a , int b){
+	if(a==0)
+	return b;
+	if(b==0)
+	return a;
+
+	while(a!=b){
+		if(a>b)
+		a = a-b;
+		else
+		b = b-a;
+	}
+	return a;
+}
+int main() {
+	int a , b;
+	cin>>a>>b;
+	int ans = gdc(a,b);
+	cout<<ans;
+	return 0;
+}
+
